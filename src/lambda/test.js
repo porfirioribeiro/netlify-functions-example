b714fca4dc4bdb7c7c360f67f02a4961418c0c18
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2018-07-22 20:57:39 +0100

    Test

--- src/lambda/test.js
+++ src/lambda/test.js
@@ -7,6 +7,9 @@ exports.handler = async (event, context) => {
     body: `
       <pre>${JSON.stringify(event, undefined, "  ")}</pre>
       <pre>${JSON.stringify(context, undefined, "  ")}</pre>      
+      <pre>${JSON.stringify(context.clientContext, undefined, "  ")}</pre>      
+      <pre>${JSON.stringify(context.identity, undefined, "  ")}</pre>      
+      <pre>${JSON.stringify(context.__proto__, undefined, "  ")}</pre>      
     `
   };
 };
