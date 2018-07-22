de38ef34dfa404457faa6d0e828e130a474d5ca4
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2018-07-22 20:45:04 +0100

    Add test

---
+++ src/lambda/test.js
@@ -0,0 +1,12 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body: `
+      <pre>${JSON.stringify(event, undefined, "  ")}</pre>
+      <pre>${JSON.stringify(context, undefined, "  ")}</pre>      
+    `
+  };
+};
